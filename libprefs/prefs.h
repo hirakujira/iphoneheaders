d915778b4e312631fbd8c80a4484f0b5e749a69d
Hiraku <dev@hiraku.tw>
2015-10-25 14:13:22 +0800

    Make libprefs compatible with iOS 9

--- libprefs/prefs.h
+++ libprefs/prefs.h
@@ -1,24 +1,4 @@
-#import <UIKit/UIKit.h>
-#import <PSListController.h>
-#import <PSSpecifier.h>
-
-
-#define PSGroupCell                     0
-#define PSLinkCell                      1
-#define PSLinkListCell                  2
-#define PSListItemCell                  3
-#define PSTitleValueCell                4
-#define PSSliderCell                    5
-#define PSSwitchCell                    6
-#define PSStaticTextCell                7
-#define PSEditTextCell                  8
-#define PSSegmentCell                   9
-#define PSGiantIconCell                 10
-#define PSGiantCell                     11
-#define PSSecureEditTextCell			12
-#define PSButtonCell                    13
-#define PSEditTextViewCell              14
-
+#import <Preferences/Preferences.h>
 
 @interface PSListController (libprefs)
 - (NSArray *)specifiersFromEntry:(NSDictionary *)entry sourcePreferenceLoaderBundlePath:(NSString *)sourceBundlePath title:(NSString *)title;
